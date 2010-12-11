36667d1d03840d59f63b64b82aad7154e0bbaa46
Darcy Laycock <sutto@sutto.net>
2010-12-12 05:18:13 +0800

    Update the layout (rough)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  
+  def ie_tag(name=:body, attrs={}, &block)
+    attrs.symbolize_keys!
+    haml_concat("<!--[if lt IE 7 ]> #{ tag(name, css_class('ie6', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 7 ]>    #{ tag(name, css_class('ie7', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 8 ]>    #{ tag(name, css_class('ie8', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 9 ]>    #{ tag(name, css_class('ie9', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if (gt IE 9)|!(IE)]><!-->".html_safe)
+    haml_tag name, attrs do
+      haml_concat("<!--<![endif]-->".html_safe)
+      block.call
+    end
+  end
+  
+  def css_class(klass, options)
+    options.merge :class => [options[:class], klass].join(' ').strip
+  end
+  
 end
