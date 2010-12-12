10c8c8dba5ce1b89cd6d985c6a6e1fb5b4e693b9
Darcy Laycock <sutto@sutto.net>
2010-12-12 16:13:24 +0800

    Setup formtastic, compass and others

---
+++ config/initializers/formtastic.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+# Formtastic::SemanticFormBuilder.default_text_field_size = 50
+# Formtastic::SemanticFormBuilder.default_text_area_height = 5
+# Formtastic::SemanticFormBuilder.default_text_area_width = 50
+# Formtastic::SemanticFormBuilder.include_blank_for_select_by_default = true
+# Formtastic::SemanticFormBuilder.required_string = "(required)"
+# Formtastic::SemanticFormBuilder.optional_string = "(optional)"
+# Formtastic::SemanticFormBuilder.inline_errors = :sentence
+# Formtastic::SemanticFormBuilder.default_hint_class = "inline-hints"
+# Formtastic::SemanticFormBuilder.default_inline_error_class = "inline-errors"
+# Formtastic::SemanticFormBuilder.default_error_list_class = "errors"
+# Formtastic::SemanticFormBuilder.label_str_method = :humanize
+# Formtastic::SemanticFormBuilder.collection_label_methods = [
+#   "to_label", "display_name", "full_name", "name", "title", "username", "login", "value", "to_s"]
+# Formtastic::SemanticFormBuilder.inline_order = [:input, :hints, :errors]
+# Formtastic::SemanticFormBuilder.custom_inline_order[:checkbox] = [:errors, :hints, :input]
+# Formtastic::SemanticFormBuilder.custom_inline_order[:select] = [:hints, :input, :errors]
+# Formtastic::SemanticFormBuilder.i18n_lookups_by_default = false
+# Formtastic::SemanticFormHelper.builder = MyCustomBuilder
