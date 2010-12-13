2a033bc869224772487087ecf45e682a467a2bfa
Darcy Laycock <sutto@sutto.net>
2010-12-13 21:54:29 +0800

    Add schema, tweak team migration

--- db/migrate/20101212125726_create_teams.rb
+++ db/migrate/20101212125726_create_teams.rb
@@ -1,13 +1,12 @@
 class CreateTeams < ActiveRecord::Migration
   def self.up
     create_table :teams do |t|
-      t.string :name
+      t.string :name, :null => false
       t.string :website_url
-      t.test :description
-      t.text :rendered_description
+      t.text   :description
+      t.text   :rendered_description
       t.string :cached_slug
       t.string :logo
-
       t.timestamps
     end
   end
