a7e6051ca17064bff495da2da79a14e91ee544f0
Darcy Laycock <sutto@sutto.net>
2010-12-28 03:46:02 +0800

    Add in cleanup for test indexing things

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,5 @@ RSpec.configure do |config|
   config.extend  DatasetCleanerHelper
   config.include Devise::TestHelpers, :type => :controller
   config.before(:each) { Machinist.reset_before_test }
+  config.after(:all)   { FileUtils.rm_rf Rails.root.join('index', Rails.env) }
 end
