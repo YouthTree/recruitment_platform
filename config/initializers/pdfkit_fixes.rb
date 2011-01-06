554acd38075918a4a49d2386c0cc909c2bf49f60
Darcy Laycock <sutto@sutto.net>
2011-01-06 21:32:58 +0800

    Add a proper fix for PDFkit

--- config/initializers/pdfkit_fixes.rb
+++ config/initializers/pdfkit_fixes.rb
@@ -3,10 +3,6 @@ PDFKit::Middleware.class_eval do
   def call_with_type_reset(env)
     call_without_type_reset env
   ensure
-    reset_env_vars!
-  end
-  
-  def reset_env_vars!
     @render_pdf = false
     @request    = nil
   end
