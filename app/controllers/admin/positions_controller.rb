09555d54873569f6f9574833b7937adebbedd3d1
Darcy Laycock <sutto@thefrontiergroup.com.au>
2011-01-13 22:37:51 +0800

    Implement the controller portion of cloning a resource

--- app/controllers/admin/positions_controller.rb
+++ app/controllers/admin/positions_controller.rb
@@ -1,7 +1,18 @@
 class Admin::PositionsController < Admin::BaseController
 
+  def clone_position(options = {}, &block)
+    flash.now[:notice] = "Cloning existing position - Please edit below and save as normal."
+    respond_with(*(with_chain(build_resource_for_cloning) << options), &block)
+  end
+
+  protected
+
   def resource
     get_resource_ivar || set_resource_ivar(end_of_association_chain.with_questions.find_using_slug!(params[:id]))
+  end
+
+  def build_resource_for_cloning
+    get_resource_ivar || set_resource_ivar(resource.clone_for_editing)
   end
 
 end
