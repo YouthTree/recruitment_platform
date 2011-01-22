4987e87c41e902e99dc8811b6839449642da54ec
Darcy Laycock <sutto@sutto.net>
2011-01-22 23:18:23 +0800

    More fixes to the migration

--- db/migrate/20110118131059_add_searchable_identifier_to_position_applications.rb
+++ db/migrate/20110118131059_add_searchable_identifier_to_position_applications.rb
@@ -3,8 +3,8 @@ class AddSearchableIdentifierToPositionApplications < ActiveRecord::Migration
     add_column :position_applications, :searchable_identifier, :string
     PositionApplication.reset_column_information
     PositionApplication.find_each do |pa|
-      next if pa.searchable_identifier.present?
       pa.update_attribute :searchable_identifier, pa.id.to_s
+      pa.update_attribute :state, 'submitted'
     end
   end
 
