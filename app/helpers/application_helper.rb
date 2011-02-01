76496024813d619ed764804c0bcb947f554c0df4
John Barker <jebarker@gmail.com>
2011-02-01 22:45:11 +0800

    Added in indicator of new position applications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,6 +38,10 @@ module ApplicationHelper
     end
   end
 
+  def new_application_count
+    PositionApplication.where(:created_at.gt => current_user.last_sign_in_at).count
+  end
+
   protected
 
   def normalized_content_scope(key, scope = nil)
