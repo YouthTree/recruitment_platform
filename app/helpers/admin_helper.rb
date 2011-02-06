d9c6a15644ad52e0803cb219dee7038069d3654c
Darcy Laycock <sutto@sutto.net>
2011-02-06 17:45:17 +0800

    make sure the new applications are only submitted

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,7 +1,7 @@
 module AdminHelper
 
   def new_application_count
-    @new_application_count ||= PositionApplication.since(current_user.last_sign_in_at).size
+    @new_application_count ||= PositionApplication.since(current_user.last_sign_in_at).submitted.size
   end
 
 end
