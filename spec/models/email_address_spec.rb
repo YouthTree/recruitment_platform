9c4429ceededd286956e5bf4c0dc0234263e7591
Darcy Laycock <sutto@sutto.net>
2011-04-13 14:30:51 +0800

    Add in spec fixes

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -13,25 +13,26 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  
+
   context 'associations' do
     specify { should belong_to :addressable, :polymorphic => true }
   end
-  
+
   context 'validations' do
+    before(:each) { EmailAddress.make!(:addressable => Position.make!) }
     specify { should validate_presence_of :email }
     specify { should allow_values_for :email,  'test@example.com', 'web@youthtree.org.au', 'sutto@sutto.net' }
     specify { should_not allow_values_for :email, 'blah', nil, '', 'test @ example.com', 'google.com'  }
     specify { should validate_uniqueness_of :email, :scope => [:addressable_type, :addressable_id], :allow_blank => true }
   end
-  
+
   context 'attribute accessibility' do
     specify { should allow_mass_assignment_of :email }
   end
-  
+
   it 'should use the email method for to_s' do
     mock(subject).email { 'sutto@sutto.net' }
     subject.to_s.should == 'sutto@sutto.net'
   end
-  
+
 end
